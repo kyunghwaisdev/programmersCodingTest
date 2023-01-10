26e6bc7640ec544948ca9b724b32c0794c5d3887
Kyunghwa <116630170+kyunghwaisdev@users.noreply.github.com>
2023-01-10 14:53:13 +0900

    [level 0] Title: 약수 구하기, Time: 1.98 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> arr = new ArrayList<>();
+        
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                arr.add(i);
+            }
+        }
+        
+        return arr.stream().mapToInt(x -> x).toArray();
+    }
+}
